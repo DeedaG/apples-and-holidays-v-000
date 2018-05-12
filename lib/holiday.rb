ed77ff4135d30f91c38b584c0781ef06e94d3c95
Christia T <christiatidwell@bellsouth.net>
2018-05-12 01:24:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     holiday.each do |supplies|
-    if supplies.include?(BBQ).to_a
+    if supplies.include?("BBQ").to_a
       return a.keys
     end
   end
